dcd0ee00a3cbfe3c88bb16aa0804eb668c05c57b
Cecep Abdul Rahman <cecep.abdul@bisnis.com>
2023-08-04 13:46:07 +0700

    Add new music

--- src/components/FloatingMusic/index.js
+++ src/components/FloatingMusic/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import IconMusic from '@assets/images/music-icon.png';
 import IconMusicStop from '@assets/images/music-stop-icon.png';
-import MusicBacksound from '@assets/music/lagu-pernikahan-impian.mp3';
+import MusicBacksound from '@assets/music/Kina Grannis & Imaginary Future - I Will Spend My Whole Life Loving You.mp3';
 
 import { styMusicFloating } from './styles';
 
