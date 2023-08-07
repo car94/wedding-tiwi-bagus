131c410ddc2def9063505f5dbaef2f36b2fc93fb
Cecep Abdul Rahman <cecep.abdul@bisnis.com>
2023-08-07 13:46:37 +0700

    Update ekstension file

--- src/components/PhotoSection/photo-data.js
+++ src/components/PhotoSection/photo-data.js
@@ -1,13 +1,13 @@
-import Gallery1 from '@assets/images/2S2A8690.jpg';
-import GalleryT1 from '@assets/images/2S2A8690.jpg';
-import Gallery2 from '@assets/images/2S2A8946.jpg';
-import GalleryT2 from '@assets/images/2S2A8946.jpg';
-import Gallery3 from '@assets/images/2S2A8533.jpg';
-import GalleryT3 from '@assets/images/2S2A8533.jpg';
-import Gallery4 from '@assets/images/2S2A8748.jpg';
-import GalleryT4 from '@assets/images/2S2A8748.jpg';
-import Gallery5 from '@assets/images/2S2A8826.jpg';
-import GalleryT5 from '@assets/images/2S2A8826.jpg';
+import Gallery1 from '@assets/images/2S2A8690.JPG';
+import GalleryT1 from '@assets/images/2S2A8690.JPG';
+import Gallery2 from '@assets/images/2S2A8946.JPG';
+import GalleryT2 from '@assets/images/2S2A8946.JPG';
+import Gallery3 from '@assets/images/2S2A8533.JPG';
+import GalleryT3 from '@assets/images/2S2A8533.JPG';
+import Gallery4 from '@assets/images/2S2A8748.JPG';
+import GalleryT4 from '@assets/images/2S2A8748.JPG';
+import Gallery5 from '@assets/images/2S2A8826.JPG';
+import GalleryT5 from '@assets/images/2S2A8826.JPG';
 
 export const photos = [
   {
