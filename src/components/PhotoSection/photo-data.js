1a8bf3da2609f03c3b72320bff714d176e91f830
Cecep Abdul Rahman <cecep.abdul@bisnis.com>
2023-08-11 22:32:04 +0700

    Add new image gallery

--- src/components/PhotoSection/photo-data.js
+++ src/components/PhotoSection/photo-data.js
@@ -1,10 +1,19 @@
-import Gallery1 from '@assets/images/gallery-1.jpg';
-import Gallery2 from '@assets/images/gallery-2.jpg';
-import Gallery3 from '@assets/images/gallery-3.jpg';
-import Gallery4 from '@assets/images/gallery-4.jpg';
-import Gallery5 from '@assets/images/gallery-5.jpg';
-import Gallery6 from '@assets/images/portrait-1.jpg';
-import Gallery7 from '@assets/images/portrait-2.jpg';
+import Gallery1 from '@assets/images/gallery-1-fix.jpg';
+import Gallery2 from '@assets/images/gallery-2-fix.jpg';
+import Gallery3 from '@assets/images/gallery-3-fix.jpg';
+import Gallery4 from '@assets/images/gallery-4-fix.jpg';
+import Gallery5 from '@assets/images/gallery-5-fix.jpg';
+import Gallery6 from '@assets/images/portrait-1-fix.jpg';
+import Gallery7 from '@assets/images/portrait-2-fix.jpg';
+import Gallery8 from '@assets/images/portrait-3-fix.jpg';
+import Gallery9 from '@assets/images/portrait-4-fix.jpg';
+import Gallery10 from '@assets/images/portrait-5-fix.jpg';
+import Gallery11 from '@assets/images/portrait-6-fix.jpg';
+import Gallery12 from '@assets/images/portrait-7-fix.jpg';
+import Gallery13 from '@assets/images/portrait-8-fix.jpg';
+import Gallery14 from '@assets/images/portrait-9-fix.jpg';
+import Gallery15 from '@assets/images/portrait-10-fix.jpg';
+import Gallery16 from '@assets/images/portrait-11-fix.jpg';
 
 export const photos = [
   {
@@ -34,5 +43,41 @@ export const photos = [
   {
     original: Gallery7,
     thumbnail: Gallery7,
+  },
+  {
+    original: Gallery8,
+    thumbnail: Gallery8,
+  },
+  {
+    original: Gallery9,
+    thumbnail: Gallery9,
+  },
+  {
+    original: Gallery10,
+    thumbnail: Gallery10,
+  },
+  {
+    original: Gallery11,
+    thumbnail: Gallery11,
+  },
+  {
+    original: Gallery12,
+    thumbnail: Gallery12,
+  },
+  {
+    original: Gallery13,
+    thumbnail: Gallery13,
+  },
+  {
+    original: Gallery14,
+    thumbnail: Gallery14,
+  },
+  {
+    original: Gallery15,
+    thumbnail: Gallery15,
+  },
+  {
+    original: Gallery16,
+    thumbnail: Gallery16,
   },
 ];
